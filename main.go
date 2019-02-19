3a0bcd7a35e0615fb9f8ec7fc4d3c8fe0036c707
ClarkGuan <guangongbo@didichuxing.com>
2019-02-19 20:27:58 +0800

    去掉对 maxSize 的支持

--- main.go
+++ main.go
@@ -40,12 +40,12 @@ func main() {
 
 	var debugLevel int
 	var bitRate int
-	var maxSize int
+	//var maxSize int
 	var port int
 	var settingFile string
 	flag.IntVar(&debugLevel, "log", 0, "日志等级设置")
 	flag.IntVar(&bitRate, "bitrate", 8000000, "视频码率")
-	flag.IntVar(&maxSize, "maxsize", 0, "未知")
+	//flag.IntVar(&maxSize, "maxsize", 0, "未知")
 	flag.IntVar(&port, "port", 27183, "adb 端口号")
 	flag.StringVar(&settingFile, "config", filepath.Join(sdl.GetBasePath(), "res", "keys.yml"), "配置文件路径")
 	flag.Parse()
@@ -101,7 +101,7 @@ func main() {
 	option := scrcpy.Option{
 		Debug:       scrcpy.DebugLevelWrap(debugLevel),
 		BitRate:     bitRate,
-		MaxSize:     maxSize,
+		MaxSize:     0,
 		Port:        port,
 		KeyMap:      keyMap,
 		CtrlKeyMap:  ctrlKeyMap,
